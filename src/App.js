6e4a43d083a374d549351909ab9710780c8e117f
AK <anttikup@metropolia.fi>
2023-04-03 17:21:32 +0300

    Initialize project using Create React App

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <textarea>
+        </textarea>
+      </header>
+    </div>
+  );
+}
+
+export default App;
